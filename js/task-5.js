84ad9f9ac7c810e7f83325bef867dfade315c610
Taras Pererva <taraspererva@gmail.com>
2025-04-26 12:20:32 +0300

    do 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+})
+
