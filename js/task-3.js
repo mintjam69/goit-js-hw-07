39b6794972ec387abdb66097c64ac4ce89128dd7
Taras Pererva <taraspererva@gmail.com>
2025-04-26 11:10:33 +0300

    3 task

--- js/task-3.js
+++ js/task-3.js
@@ -5,4 +5,20 @@ nameInput.addEventListener('input', () => {
     const trimmedValue = nameInput.value.trim();
     nameOutput.textContent = trimmedValue !== '' ? trimmedValue : 'Anonymous';
 });
-  
+
+
+/* Без тернарного оператора */
+
+
+// function updateGreeting() {
+//     const trimedValue = nameInput.value.trim();
+//     if (trimedValue === '') {
+//         nameOutput.textContent = "Anonymous";
+//     } else {
+//         nameOutput.textContent = trimedValue;
+//     }
+// }
+// nameInput.addEventListener("input", updateGreeting);
+
+
+/* /Без тернарного оператора */
